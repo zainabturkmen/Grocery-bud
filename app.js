5c217c74052116816de518f112a58ec78734eda6
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-23 14:26:02 +0430

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+// ****** SELECT ITEMS **********
+
+// edit option
+
+// ****** EVENT LISTENERS **********
+
+// ****** FUNCTIONS **********
+
+// ****** LOCAL STORAGE **********
+
+// ****** SETUP ITEMS **********
+ 
