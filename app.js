49786f0804fff705386eaa6f2a841f891cf87a26
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-24 07:08:05 +0430

    adding some changes

--- app.js
+++ app.js
@@ -2,6 +2,10 @@
  const alert = document.querySelector(".alert");
  const form = document.querySelector(".grocery-form");
  const grocery = document.querySelector(".grocery");
+ const submitBtn = document.querySelector(".submit-btn");
+ const groceryContainer = document.querySelector(".grocery-container");
+ const groceryList = document.querySelector(".grocery-list");
+ const clearBtn = document.querySelector(".clear-btn");
  
 // edit option
 
