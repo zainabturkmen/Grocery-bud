ee4b2be2b55b7c2e3332593016fef99300119ae4
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-24 10:07:12 +0430

    adding some changes

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ function addItem(e){
         const attr = document.createAttribute("data-id");
         attr.value = id;
         element.setAttributeNode(attr);
-        element.innerHTML =   ` <p class="title">item</p>
+        element.innerHTML =   ` <p class="title">${value}</p>
                     <div class="btn-continer">
                     <button type="button" class="edit-btn">
                         <i class="fas fa-edit"></i>
@@ -70,12 +70,15 @@ function displayAlert(text, action){
 }
 // set back to default
 function setBackToDefault(){
-
+    grocery.value = "";
+    editFlag = false;
+    editElement = "";
+    submitBtn.textContent = "submit";
 };
 
 // ****** LOCAL STORAGE **********
 function addToLocalStorage(id, value){
-    
+    console.log("added to local storage");
 };
 // ****** SETUP ITEMS **********
  
