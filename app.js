4cd16506339c52540d89ca1b241ee4e9dbc52391
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-24 08:45:21 +0430

    adding some changes

--- app.js
+++ app.js
@@ -28,14 +28,20 @@ function addItem(e){
         console.log("editing")
     } 
     else{
-        alert.textContent = "Empty Value"
-        alert.classList.add("alert-danger")
+       displayAlert("please enter value", "danger");
     }
 }
 
 // display alert
-function displayAlert(){
-    
+function displayAlert(text, action){
+    alert.textContent = text;
+    alert.classList.add(`alert-${action}`);
+
+    setTimeout(function(){
+        alert.textContent = "";
+        alert.classList.remove(`alert-${action}`);
+    }, 1000)
+
 }
 // ****** LOCAL STORAGE **********
 
